031ec8991ebe6811f2dbbac3d9951a59d8bb0e55
baabak aghaei <baabakaghaei@gmail.com>
2023-09-08 16:21:46 +0330

    Add project files.

---
+++ GymManegmentSystem.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymManegmentSystem.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GymManegmentSystemDomin/Entity/Membership/MemberShip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymManegmentSystemDomin.Common;
+
+namespace GymManegmentSystemDomin.Entity.Membership
+{
+    public class MemberShip:BaseEntity
+    {
+        public MembershipType Name { get; set; }
+        public int Duration { get; set; }
+        public double Price { get; set; }
+
+        
+        #region Relation
+
+        public MemberShipDetails MemberShipDetails { get; set; }
+
+        #endregion relation
+
+    }
+
+    public enum MembershipType
+    {
+        Golden,
+        Silver,
+        Bronze,
+        Daily
+    }
+}
