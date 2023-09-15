63f6c9bfbc2f12fb3704867fdd7a0af39abc9270
baabak aghaei <baabakaghaei@gmail.com>
2023-09-15 20:08:42 +0330

    adding mobile project

---
+++ GymManegmentSystemMobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GymManegmentSystemMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GymManegmentSystemMobileApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GymManegmentSystemMobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
