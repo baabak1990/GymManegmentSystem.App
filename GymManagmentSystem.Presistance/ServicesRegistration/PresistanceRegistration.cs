a6ebc31d2e2c7190ce97d860050a72c63514a08b
baabak aghaei <baabakaghaei@gmail.com>
2023-09-16 19:11:08 +0330

    Repository Second

---
+++ GymManagmentSystem.Presistance/ServicesRegistration/PresistanceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymManagmentSystem.Presistance.Context;
+using GymManagmentSystem.Presistance.Repositories;
+using GymManegmentApplication.Contracts.Presistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GymManagmentSystem.Presistance.ServicesRegistration
+{
+    public static class PresistanceRegistration
+    {
+        public static IServiceCollection PresistanceServiceCollection(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<GymManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(""));
+            });
+
+            services.AddScoped( typeof(IGenericRepository<>),typeof(GenericRepository<>));
+
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddScoped<IMemberShipRepository, MembershipRepository>();
+
+            return services;
+        }
+    }
+}
